3c768d07d4a1544eaf8e134d9d843a239032a0ad
Isaacpyo <temitopeagbola@gmail.com>
2020-05-02 22:49:21 +0100

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,59 +1,89 @@
-jQuery(document).ready(function ($) {
-  "use strict";
+/*  ---------------------------------------------------
+    Template Name: Local Direction
+    Description: Local Direction HTML Template
+    Author: Colorlib
+    Author URI: https://www.colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
 
-  // siteSliderRange();
+"use strict";
 
-  var siteCarousel = function () {
-    if ($(".nonloop-block-13").length > 0) {
-      $(".nonloop-block-13").owlCarousel({
-        center: false,
-        items: 1,
-        loop: true,
-        stagePadding: 0,
-        margin: 0,
-        autoplay: true,
-        nav: true,
-        navText: [
-          '<span class="icon-arrow_back">',
-          '<span class="icon-arrow_forward">',
-        ],
-        responsive: {
-          600: {
-            margin: 0,
-            nav: true,
-            items: 2,
-          },
-          1000: {
-            margin: 0,
-            stagePadding: 0,
-            nav: true,
-            items: 3,
-          },
-          1200: {
-            margin: 0,
-            stagePadding: 0,
-            nav: true,
-            items: 4,
-          },
-        },
-      });
+(function ($) {
+  /*------------------
+      Preloader
+  --------------------*/
+
+  /*------------------
+      Background Set
+  --------------------*/
+  $(".set-bg").each(function () {
+    var bg = $(this).data("setbg");
+    $(this).css("background-image", "url(" + bg + ")");
+  });
+
+  /*----------------------
+        Testimonial Slider
+    -----------------------*/
+  // $(".testimonial-item").owlCarousel({
+  //   loop: true,
+  //   margin: 0,
+  //   nav: true,
+  //   items: 1,
+  //   dots: false,
+  //   navText: [
+  //     '<i class="fa fa-angle-left"></i>',
+  //     '<i class="fa fa-angle-right"></i>',
+  //   ],
+  //   smartSpeed: 1200,
+  //   autoplay: false,
+  // });
+
+  /*-------------------
+  Category Select
+--------------------- */
+  $(".ca-search").niceSelect();
+
+  /*-------------------
+  Local Select
+--------------------- */
+  $(".lo-search").niceSelect();
+
+  /*-------------------
+  Arrange Select
+--------------------- */
+  $(".arrange-select select").niceSelect();
+
+  /*-------------------
+  Radio Btn
+--------------------- */
+  $(".filter-left .category-filter .category-option .co-item label").on(
+    "click",
+    function () {
+      $(
+        ".filter-left .category-filter .category-option .co-item label"
+      ).removeClass("active");
+      $(this).addClass("active");
     }
+  );
 
-    $(".slide-one-item").owlCarousel({
-      center: false,
-      items: 1,
-      loop: true,
-      stagePadding: 0,
-      margin: 0,
-      smartSpeed: 1000,
-      autoplay: true,
-      pauseOnHover: false,
-      nav: true,
-      navText: [
-        '<span class="icon-keyboard_arrow_left">',
-        '<span class="icon-keyboard_arrow_right">',
-      ],
-    });
-  };
-  siteCarousel();
-});
+  $(".filter-left .rating-filter .rating-option .ro-item label").on(
+    "click",
+    function () {
+      $(
+        ".filter-left .rating-filter .rating-option .ro-item label"
+      ).removeClass("active");
+      $(this).addClass("active");
+    }
+  );
+
+  $(".filter-left .distance-filter .distance-option .do-item label").on(
+    "click",
+    function () {
+      $(
+        ".filter-left .distance-filter .distance-option .do-item label"
+      ).removeClass("active");
+      $(this).addClass("active");
+    }
+  );
+})(jQuery);
