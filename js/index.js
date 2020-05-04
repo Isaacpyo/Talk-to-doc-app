aadc472e1225cd7fdf15d2b36b33857ca4b51376
Isaacpyo <temitopeagbola@gmail.com>
2020-05-04 18:15:59 +0100

    Sixth commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+// DOM elements
+const guideList = document.querySelector(".guides");
+
+// setup guides
+const setupGuides = (data) => {
+  let html = "";
+  data.forEach((doc) => {
+    const doctors = doc.data();
+    console.log(doctors);
+    const li = `
+      <li>
+        <div class="collapsible-header grey lighten-4"> ${doctors.categories} </div>
+        <div class="collapsible-body white"> ${doctors.content} </div>
+      </li>
+    `;
+    html += li;
+  });
+  guideList.innerHTML = html;
+};
+
 // setup materialize components
 document.addEventListener("DOMContentLoaded", function () {
   var modals = document.querySelectorAll(".modal");
